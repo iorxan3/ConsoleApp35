b6ead9932c31786852543ac04f4000d944bbafbe
Orxan <ibrahimzada003@gmail.com>
2023-11-26 18:40:09 +0400

    Add project files.

---
+++ ConsoleApp35/Models/K.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp35.Models
+{
+    public class K
+    {
+        public int Kelvin { get; set; }
+        public K(int kelvin)
+        {
+            Kelvin = kelvin;
+        }
+        public static implicit operator C(K kelvinn)
+        {
+            return new C(kelvinn.Kelvin - 273);
+        }
+    }
+}
---
+++ ConsoleApp35/Program.cs
@@ -0,0 +1,5 @@
+﻿using ConsoleApp35.Models;
+
+K kelvin = new(10);
+C celsi = kelvin;
+Console.WriteLine(celsi.Celsi);
